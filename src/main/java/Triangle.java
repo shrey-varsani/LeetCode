9080a026301c67a7002019b55ed94f7cd91fcd51
shrey-varsani <shreyvarsani28@gmail.com>
2026-08-05 18:06:33 +0530

    Refactor Triangle class to use Integer array for memoization and implement tabulation methods for minimum path sum calculation

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -5,19 +5,22 @@ public class Triangle {
     public int minPath(List<List<Integer>> triangle) {
         int len = triangle.size();
 
-        int[][] dp = new int[len][len];
+        Integer[][] dp = new Integer[len][len];
+        for(Integer[] each: dp) {
+            Arrays.fill(each, -1);
+        }
 
         return solver(triangle, dp, 0, 0);
     }
 
-    public int solver(List<List<Integer>> triangle, int[][] dp, int i, int j) {
+    public int solver(List<List<Integer>> triangle, Integer[][] dp, int i, int j) {
         int len = triangle.size();
 
         if(i == len - 1) {
             return triangle.get(i).get(j);
         }
 
-        if(dp[i][j] != -1) {
+        if(dp[i][j] != null) {
             return dp[i][j];
         }
 
@@ -28,5 +31,39 @@ public class Triangle {
         return dp[i][j];
     }
 
-    
+    public int minPathTab(List<List<Integer>> triangle) {
+        int len = triangle.size();
+
+        int[][] dp = new int[len][len];
+
+        for(int i=0; i<len; i++) {      // copy the last row first
+            dp[len-1][i] = triangle.get(len-1).get(i);
+        }
+
+        for(int i=len-2; i>=0; i--) {
+            for(int j=0; j<=i; j++) {
+                dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle.get(i).get(j);           // get min from same col and next col in the next row
+            }
+        }
+
+        return dp[0][0];
+    }
+
+    public int minPathTabSpaceOpt(List<List<Integer>> triangle) {
+        int len = triangle.size();
+
+        int[] dp = new int[len];
+
+        for(int i=0; i<len; i++) {      // copy the last row first
+            dp[i] = triangle.get(len-1).get(i);
+        }
+
+        for(int i=len-2; i>=0; i--) {
+            for(int j=0; j<=i; j++) {
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);           // get min from same col and next col in the next row
+            }
+        }
+
+        return dp[0];
+    }
 }
