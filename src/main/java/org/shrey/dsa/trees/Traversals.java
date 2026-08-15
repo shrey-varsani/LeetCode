7318cc866bc847b5171e079d804c5164405bd67b
shrey-varsani <shreyvarsani28@gmail.com>
2026-08-15 14:45:08 +0530

    Add allInOne method to Traversals class for simultaneous pre-order, in-order, and post-order traversal; implement post-order traversal methods in TraversalsStack class

--- src/main/java/org/shrey/dsa/trees/Traversals.java
+++ src/main/java/org/shrey/dsa/trees/Traversals.java
@@ -1,7 +1,6 @@
 package org.shrey.dsa.trees;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Traversals {
     public void preOrder(Node root) {
@@ -48,5 +47,73 @@ public class Traversals {
                 que.offer(curr.right);
             }
         }
+    }
+
+    public void allInOneRecur(Node root, List<Integer> pre, List<Integer> in, List<Integer> post) {
+        if(root == null) return;
+        pre.add(root.val);
+
+        allInOneRecur(root.left, pre, in, post);
+
+        in.add(root.val);
+
+        allInOneRecur(root.right, pre, in, post);
+
+        post.add(root.val);
+    }
+
+    public List<List<Integer>> allInOne(Node root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) return res;
+
+        List<Integer> pre = new ArrayList<>();
+        List<Integer> in = new ArrayList<>();
+        List<Integer> post = new ArrayList<>();
+
+        Stack<Pair> stk = new Stack<>();
+        stk.add(new Pair(root, 1));
+
+        while(!stk.isEmpty()) {
+            Pair curr = stk.pop();
+            Node currNode = curr.node;
+            int currPos = curr.pos;
+
+            switch(currPos) {
+                case 1 -> {
+                    pre.add(currNode.val);
+                    currPos++;
+                    stk.push(curr);
+
+                    if(currNode.left != null) {
+                        stk.push(new Pair(currNode.left, 1));
+                    }
+                }
+                case 2 -> {
+                    in.add(currNode.val);
+                    currPos++;
+                    stk.push(curr);
+
+                    if(currNode.right != null) {
+                        stk.push(new Pair(currNode.right, 1));
+                    }
+                }
+                default -> post.add(currNode.val);
+            }
+        }
+
+        res.add(pre);
+        res.add(in);
+        res.add(post);
+        return res;
+    }
+}
+
+class Pair {
+    Node node;
+    int pos;
+
+    public Pair(Node root, int pos){
+        this.node = root;
+        this.pos = pos;
     }
 }
--- src/main/java/org/shrey/dsa/trees/TraversalsStack.java
+++ src/main/java/org/shrey/dsa/trees/TraversalsStack.java
@@ -39,6 +39,58 @@ public class TraversalsStack {
 
         return res;
     }
+
+    public List<Integer> postOrder2Stacks(Node root) {
+        Stack<Node> s1 = new Stack<>();
+        Stack<Node> s2 = new Stack<>();
+
+        List<Integer> res = new ArrayList<>();
+
+        Node curr = root;
+        s1.push(curr);
+        while(!s1.isEmpty()) {
+            curr = s1.pop();
+
+            s2.push(curr);      // before adding children add that node in 2nd stack
+
+            if(curr.left != null) s1.push(curr.left);
+            if(curr.right != null) s1.push(curr.right);
+        }
+
+        while(!s2.isEmpty()) {
+            res.add(s2.pop().val);
+        }
+
+        return res;
+    }
+
+    public List<Integer> postOrder1Stack(Node root) {
+        List<Integer> res = new ArrayList<>();
+
+        if(root == null) return res;
+        Stack<Node> stk = new Stack<>();
+
+        Node curr = root;
+        Node lastVisit = null;
+        stk.push(curr);
+
+        while(!stk.isEmpty()) {
+            if(curr != null) {
+                stk.push(curr);
+                curr = curr.left;
+            }
+
+            Node peek = stk.pop();
+            if(peek.right != null && lastVisit != peek.right) {
+                curr = peek.right;
+            } else {
+                res.add(peek.val);
+                lastVisit = stk.pop();
+            }
+        }
+
+        return res;
+    }
 }
 
 
