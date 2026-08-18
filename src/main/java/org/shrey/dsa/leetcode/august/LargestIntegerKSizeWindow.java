8097f20b2f52d9dd1d1d4c70899ea8604a4f6540
shrey-varsani <shreyvarsani28@gmail.com>
2026-08-18 18:43:41 +0530

    Changed the from map to freq[] array only

--- src/main/java/org/shrey/dsa/leetcode/august/LargestIntegerKSizeWindow.java
+++ src/main/java/org/shrey/dsa/leetcode/august/LargestIntegerKSizeWindow.java
@@ -4,45 +4,26 @@ import java.util.*;
 
 public class LargestIntegerKSizeWindow {
     public int largestInteger(int[] arr, int k) {
-        int len = arr.length;
-        int max = Integer.MIN_VALUE;
-        
         int[] freq = new int[51];
-        Map<Integer, Integer> map = new HashMap<>();
 
-        for(int i=0; i<k; i++) {
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
-        }
+        for(int i = 0; i <= arr.length - k; i++) {
 
-        for(int key: map.keySet()) {
-            freq[key]++;
-        }
+            boolean[] present = new boolean[51];
 
-        // k to len
-        for(int i=k; i<len; i++) {
-            int remove = arr[i - k];
-            map.put(remove, map.get(remove) - 1);
-
-            if(map.get(remove) == 0) {
-                map.remove(remove);
+            for(int j = i; j < i + k; j++) {
+                present[arr[j]] = true;
             }
 
-            int add = arr[i];
-
-            if(!map.containsKey(add)) {
-                map.put(add, 1);
-
-                freq[add]++;
-            } else {
-                map.put(add, map.getOrDefault(add, 0) + 1);
+            for(int x = 0; x <= 50; x++) {
+                if(present[x]) {
+                    freq[x]++;
+                }
             }
-            
         }
 
-        for(int i=freq.length-1; i>=0; i--) {
-            if(freq[i] == 1) {
-               return i;
-                // whose freq is 1 
+        for(int x = 50; x >= 0; x--) {
+            if(freq[x] == 1) {
+                return x;
             }
         }
 
