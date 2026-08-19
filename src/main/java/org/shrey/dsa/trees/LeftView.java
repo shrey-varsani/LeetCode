a88374ecba2ec7612ab8b71c56a08fbd2d22e8e3
shrey-varsani <shreyvarsani28@gmail.com>
2026-08-19 21:27:45 +0530

    Add LeftView and RightView classes for tree traversal; implement left and right view logic

---
+++ src/main/java/org/shrey/dsa/trees/LeftView.java
@@ -0,0 +1,34 @@
+package org.shrey.dsa.trees;
+
+import java.util.*;
+
+public class LeftView {
+    public List<Integer> leftView(Node root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null) return res;
+
+        Queue<Node> que = new LinkedList<>();
+        que.offer(root);
+
+        while(!que.isEmpty()) {
+            int currSize = que.size();
+
+            for(int i=0; i<currSize; i++) {
+                Node curr = que.poll();
+                if(i == 0) {
+                    res.add(curr.val);
+                }
+
+                if(curr.left != null) {
+                    que.offer(curr.left);
+                }
+
+                if(curr.right != null) {
+                    que.offer(curr.right);
+                }
+            }
+        }
+
+        return res;
+    }
+}
